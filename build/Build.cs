0b1cf8e424d1262b89a8ade06ab7e4d36f016407
Gurdip <gurdip.sira@gmail.com>
2024-06-16 00:56:24 +0100

    feat(PulumiStateImporter): :sparkles: s
    s
    
    BREAKING CHANGE: s

---
+++ build/Build.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter("Name")]
+    readonly string Name;
+
+    [Parameter("Id")]
+    readonly string Id;
+
+    [Parameter("Resource")]
+    readonly string Resource;
+
+
+    [PathVariable]
+    readonly Tool Pulumi;
+
+    Target Import => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            Pulumi($"import {Resource}  {Name} {Id}");
+        });
+
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+        });
+
+}
